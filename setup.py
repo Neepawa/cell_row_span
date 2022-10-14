91b169b5e0ae8fbe9949a54efa18d2ab5ef90c77
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2022-10-14 15:41:56 +0200

    Bump v1.0.0
    * Use Python-Markdown v3.x
    * Fix deprecation warnings with Python-Markdown v3.x
    * Format files with autopep8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='cell_row_span',
-    version='1.0',
+    version='1.0.0',
     description='Markdown extension to add spanning for rows and cells in tables.',
     url='https://github.com/Neepawa/cell_row_span',
     py_modules=['cell_row_span'],
