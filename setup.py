60e6ef08c460a7d52a2187c43b23a4bcd2a8b934
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2022-10-14 15:41:06 +0200

    Format files with autopep8

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from distutils.core import setup
 
-setup(name='cell_row_span',
-      version='1.0',
-      description='Markdown extension to add spanning for rows and cells in tables.',
-      url='https://github.com/Neepawa/cell_row_span',
-      py_modules=['cell_row_span'],
-      install_requires=['Markdown>=3.0'],
-      )
+setup(
+    name='cell_row_span',
+    version='1.0',
+    description='Markdown extension to add spanning for rows and cells in tables.',
+    url='https://github.com/Neepawa/cell_row_span',
+    py_modules=['cell_row_span'],
+    install_requires=['Markdown>=3.0'],
+)
